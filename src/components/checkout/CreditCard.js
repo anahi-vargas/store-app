9c670ce6c76574ecaf210f600ef116da42889ed1
anahi-vargas <anahiv643@gmail.com>
2022-02-18 12:48:31 -0800

    fix: credit card form

--- src/components/checkout/CreditCard.js
+++ src/components/checkout/CreditCard.js
@@ -14,7 +14,7 @@ function CreditCard(props) {
         <FormInput required placeholder="Cardholder Name" name="cardholderName" value={cardholderName} onChange={handleCard} />
         <FormInput minLength={16} maxLength={16} placeholder="Credit Card Number" name="cardNumber" value={cardNumber} onChange={handleCard} />
         <FormRow>
-            <FormInput minLength={3} maxLength={3} placeholder="Security Code" name="securityCode" value={securityCode} onChange={handleCard}  />
+            <FormInput minLength={3} maxLength={4} placeholder="CVV" name="securityCode" value={securityCode} onChange={handleCard}  />
             <FormInput type="month" min="2022-02" max="2030-01" pattern="[0-9]{4}-[0-9]{2}" placeholder="Expiration Date" name="expirationDate" value={expirationDate} onChange={handleCard}  />
         </FormRow>
         </>
